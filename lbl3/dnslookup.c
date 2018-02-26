75ce7fc05659e2e6ad81bf51b8af54e346ed02ad
Nicky Vo <2253715v@student.gla.ac.uk>
2018-02-26 18:40:14 +0000

    Don't know what happen but part 1 is finished

--- lbl3/dnslookup.c
+++ lbl3/dnslookup.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
     int i, fd;
     char buf[BUFLEN];
-    const char *crrAdd;
+    void *crrAdd;
 
     int loopIndex;
     if (argc < 2)
@@ -25,31 +25,52 @@ int main(int argc, char *argv[])
         printf("Usage: %s <hostname(s)>\n", argv[0]);
         return 1;
     }
-    //Loop through domains
-    for (loopIndex = 1; loopIndex <= argc; loopIndex++)
-    {
-        // Look up the IP addresses
-        memset(&hints, 0, sizeof(hints));
+
+    memset(&hints, 0, sizeof(hints));
         hints.ai_family = PF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
+    //Loop through domains
+    for (loopIndex = 1; loopIndex < argc; loopIndex++)
+    {
+        // Look up the IP addresses     
 
-        if ((i = getaddrinfo(argv[1], "80", &hints, &ai0)) != 0)
+        if ((i = getaddrinfo(argv[loopIndex], "5000", &hints, &ai0)) != 0)
         {
             printf("Unable to look up IP address: %s", gai_strerror(i));
             continue;
         }
+        int ipFam =-1;
+        
+
         //printing the IP strings
         for (ai = ai0; ai != NULL; ai = ai->ai_next)
         {
-            crrAdd = inet_ntop(ai->ai_family, ai->ai_addr, buf, sizeof(buf));
-            if (crrAdd == 0)
+            //Check if IPv4/v6
+            if(ai->ai_family == AF_INET)//IPv4
             {
-                printf("failed to convert address to string (errno=%d)", errno);
-                continue; // Try the next address
-            }
+                ipFam =4;
+                crrAdd = &((struct sockaddr_in *)ai->ai_addr)->sin_addr;
 
-            // Print out the address
+            }else if (ai->ai_family == AF_INET6)//IPv6
+            {
+                ipFam =6;
+                crrAdd = &((struct sockaddr_in6 *)ai->ai_addr)->sin6_addr;
+            }
+            inet_ntop(ai->ai_family, crrAdd, buf, sizeof(buf));
+            // if (crrAdd == 0)
+            // {
+            //     printf("failed to convert address to string (errno=%d)", errno);
+            //     continue; // Try the next address
+            // }
+            printf("%s IPv%d %s\n",argv[loopIndex],ipFam,buf);
+
+            
         }
         //FREE THE VARS
+        freeaddrinfo(ai0);
+        
     }
+    free(ai);
+    free(ai0);
+    free(crrAdd);
 }
